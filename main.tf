8c8ea3fdebbe0e251d5481914ec42522a3f5a94a
sham <root@ip-172-31-5-218.us-west-2.compute.internal>
2024-04-29 05:02:26 +0000

    second commit-jenkins-ec2 added

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ resource "aws_vpc" "Test-VPC" {
     Name = "Test-VPC"
   }
 }
+
+resource "aws_instance" "jenkins-ec2" {
+  ami = var.ami-linux-us-west-2
+  instance_type = var.instance_type
+  security_groups = [ var.sg-all-allowed ]
+  subnet_id = var.public-az-a
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "jenkins-ec2"
+  }
+}
